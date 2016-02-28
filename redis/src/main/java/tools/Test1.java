792dc9715b7449cecf9e345329217a2d63d70e11
yuhao <569425720@qq.com>
2016-02-28 19:11:31 +0800

    teet1

---
+++ redis/src/main/java/tools/Test1.java
@@ -0,0 +1,10 @@
+package tools;
+
+/**
+ * Created by yuhao.zx on 16-2-28.
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("good work hehe hehe hehe");
+    }
+}
